cabfe1c03b0d3403917fb3b46f9bed69b556e135
santiago <sansantitiagoago@gmail.com>
2022-04-05 13:27:58 -0300

    Filtro de perfeitas paralelas (em desenvolvimento)

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ dorico = [50, 53, 52, 50, 55, 53, 57, 55, 53, 52, 50]
 frigio = [52, 48, 50, 48, 45, 57, 55, 52, 53, 52]
 lidio = [53, 55, 57, 53, 50, 52, 53, 60, 57, 53, 55, 53]
 mixo = [43, 48, 47, 43, 48, 52, 50, 55, 52, 48, 50, 47, 45, 43]
-eolio = [57, 60, 59, 62, 60, 64, 65, 64, 62, 60, 59, 57]
+eolio = [45, 48, 47, 50, 48, 52, 53, 52, 50, 48, 47, 45]
 cf = []
 naturais = [36, 38, 40, 41, 43, 45, 47, 48,	50,	52,	53,	55,	57,	59, 60,	62,	64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84]
-int_cons = [3, 4, 7, 8, 9, 12, 15, 16, 19, 20, 21]
+int_cons = [0, 3, 4, 7, 8, 9, 12, 15, 16, 19, 20, 21]
+conper = [0, 7, 8, 19]
 #naipes
 s = {"mi" : 60, "ma" : 79, "clave" : "soprano"}
 a = {"mi" : 55, "ma" : 74, "clave" : "alto"}
@@ -28,14 +29,11 @@ class CF:
             return "bass"
         else:
             return "alto"
-
 class RES:
     def __init__(self, naipe):
         self.naipe = naipe
     def notas(self):
         self.notas = []
-
-
 def cf_auto():
     global cf
     global contra
@@ -64,49 +62,47 @@ def cf_auto():
         contra = RES(t)
     if entrada2 == "b":
         contra = RES(b)
-
 #print
-def print_prim_esp(cf, res):
+def print_prim_esp(cf, contra, res):
     if cf.naipe == 1:
-        print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff {\n\\clef ", res.naipe["clave"])
-        for i in range(len(res.notas)):
-            print(midi_ly[res.notas[i]], "1", sep="", end=" ")
+        print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff {\n\\clef ", contra.naipe["clave"])
+        for i in range(len(res)):
+            print(midi_ly[res[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
                 print("\n}\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef bass")
         for i in range(len(cf.modo)):
             print(midi_ly[cf.modo[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
-                print("\n}\n>>\n")
+                print("\n}\n>>")
     else:
         print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef alto")
         for i in range(len(cf.modo)):
             print(midi_ly[cf.modo[i] + 12], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
-                print("\n}\n\\new Staff {\n\\clef ", res.naipe["clave"])
-        for i in range(len(res.notas)):
-            print(midi_ly[res.notas[i]], "1", sep="", end=" ")
+                print("\n}\n\\new Staff {\n\\clef ", contra.naipe["clave"])
+        for i in range(len(res)):
+            print(midi_ly[res[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
-                print("\n}\n>>\n")
-
-
+                print("\n}\n>>")
 #quantizador
-def quant(nota):
-    if nota not in naturais:
-        return nota + 1
+def gerar_nota(contra):
+    nota2 = random.randint(contra.naipe["mi"], contra.naipe["ma"])
+    if nota2 not in naturais:
+        return nota2 + 1
     else:
-        return nota
+        return nota2
 
-#verificações
-def verif_cons(nota1, nota2):
-    if abs(nota1 - nota2) in int_cons:
-        return True
-    else:
-        return False
-def verif_tess(nota, naipe):
+#filtros
+def filt_tess(nota, naipe):
     if nota > naipe["ma"] or nota < naipe["mi"]:
-        return False
-    else:
         return True
+def filt_diss(nota1, nota2):
+    if abs(nota1 - nota2) not in int_cons:
+        return True
+def filt_pll(i, cf, res, nota):
+    if abs(cf[i - 1] - res[i - 1]) and abs(cf[i] - nota) in conper:
+        return True
+
 
 #gera primeira espécie
 def main(cf, contra):
@@ -121,42 +117,41 @@ def main(cf, contra):
         #primeira nota
         if cf.naipe == 1:
             nota2 = random.randint(0, 1) * 12 + cf.modo[0]
-            if verif_tess(nota2, contra.naipe):
+            if not filt_tess(nota2, contra.naipe):
                 res.append(nota2)
         else:
             nota2 = random.randint(-1, 0) * 12 + cf.modo[0]
-            if verif_tess(nota2, contra.naipe):
+            if not filt_tess(nota2, contra.naipe):
                 res.append(nota2)
 
         for i in range(len(cf.modo)):
             i += 1
+
             #última nota
-            if i == len(cf.modo) - 1:
-                    res.append(res[0])
-                    completou = True
-                    print("\n")
-                    break
+            if len(res) == len(cf.modo) - 1:
+                res.append(res[0])
+                completou = True
+                print("\n")
+                print(f"tentativas: {it}")
+                break
 
             else:
-                nota2 = random.randint(contra.naipe["mi"], contra.naipe["ma"])
-                nota2 = quant(nota2)
-                if verif_cons(cf.modo[i], nota2):
-                    res.append(nota2)
-
-                #nova tentativa
-                else:
+                nota2 = gerar_nota(contra)
+                #filtros
+                if filt_diss(cf.modo[i], nota2):
                     it += 1
                     print(".", end="")
                     break
-        if it >= 100000:
+
+                else:
+                    res.append(nota2)
+        if it >= 1000000000:
             print("\nExcedeu limite de iterações")
             break
-    print("resolução:     ", res)
-    print("cantus firmus: ", cf.modo)
-
-    contra.notas = res
 
 cf_auto()
 print("\n", cf.modo, cf.naipe, contra.naipe,"\n")
 main(cf, contra)
-print_prim_esp(cf, contra)
+print(f"cantus firmus:{cf.modo}")
+print(f"resolução:    {res}")
+print_prim_esp(cf, contra, res)
--- teste.py
+++ teste.py
@@ -1,17 +1,8 @@
 lista1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+import random
 
+def gerar_nota():
+    return random.randint(0, 10)
 
-lista2 =[]
-completou = False
-
-while completou == False:
-    lista2.append("a")
-    for i in range(len(lista1)):
-        i += 1
-        if i == len(lista1):
-            lista2.append(lista2[0])
-            print(lista2)
-            completou = True
-            break
-        else:
-            lista2.append(i)
+nota = gerar_nota()
+print(nota)
