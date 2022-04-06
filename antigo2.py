8d1535edf4b28a1238f90b8942a379e8cb25d796
santiago <sansantitiagoago@gmail.com>
2022-04-06 14:57:05 -0300

    Classe RES resolvida; filtro de paralelismo

--- antigo2.py
+++ antigo2.py
@@ -32,12 +32,8 @@ class CF:
 class RES:
     def __init__(self, naipe):
         self.naipe = naipe
+    def notas(self):
         self.notas = []
-    def add(self, nota):
-        self.notas.append(nota)
-    def clear(self):
-        self.notas = []
-
 def cf_auto():
     global cf
     global contra
@@ -67,11 +63,11 @@ def cf_auto():
     if entrada2 == "b":
         contra = RES(b)
 #print
-def print_prim_esp(cf, contra):
+def print_prim_esp(cf, contra, res):
     if cf.naipe == 1:
         print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff {\n\\clef ", contra.naipe["clave"])
-        for i in range(len(contra.notas)):
-            print(midi_ly[contra.notas[i]], "1", sep="", end=" ")
+        for i in range(len(res)):
+            print(midi_ly[res[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
                 print("\n}\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef bass")
         for i in range(len(cf.modo)):
@@ -84,8 +80,8 @@ def print_prim_esp(cf, contra):
             print(midi_ly[cf.modo[i] + 12], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
                 print("\n}\n\\new Staff {\n\\clef ", contra.naipe["clave"])
-        for i in range(len(contra.notas)):
-            print(midi_ly[contra.notas[i]], "1", sep="", end=" ")
+        for i in range(len(res)):
+            print(midi_ly[res[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
                 print("\n}\n>>")
 #quantizador
@@ -96,9 +92,9 @@ def gerar_nota(contra):
     else:
         return nota2
 #intervalo
-def int_har(nota1, nota2):
+def int_abs(nota1, nota2):
     return abs(nota1 - nota2)
-def int_mel(nota, notal):
+def int_rel(nota, notal):
     return notal - nota
 
 #filtros
@@ -108,14 +104,24 @@ def filt_tess(nota, naipe):
 def filt_diss(nota1, nota2):
     if abs(nota1 - nota2) not in int_cons:
         return True
+#def filt_pll(i, cf, res, nota):
+#    if abs(cf[i - 1] - res[i - 1]) and abs(cf[i] - nota) in conper:
+#        return True
+#
+#    #filtra consocâncias perfeitas por movimento direto
+#    if nota - res[i - 1] > 0 and cf[i] - cf[i - 1] > 0:
+#        if abs(cf[i] - nota) in conper:
+#            return True
+#    if nota - res[i - 1] < 0 and cf[i] - cf[i - 1] < 0:
+#        if abs(cf[i] - nota) in conper:
+#            return True
 def filt_pll(i, cf, res, nota):
-    if int_har(cf[i - 1], res[i - 1]) in conper:
-        if int_har(cf[i], nota) in conper:
+    if int_abs(cf[i], nota) in conper:
+        if int_abs(cf[i - 1], res[i - 1]) in conper:
             return True
-
-        if int_mel(res[i - 1], nota) >= 1 and int_mel(cf[i - 1], cf[i]) >= 1:
+        if int_rel(res[i - 1], nota) > 0 and int_rel(cf[i - 1], cf[i]) > 0:
             return True
-        if int_mel(res[i - 1], nota) <= -1 and int_mel(cf[i - 1], cf[i]) <=-1:
+        if int_rel(res[i - 1], nota) < 0 and int_rel(cf[i - 1], cf[i]) < 0:
             return True
 
 
@@ -126,43 +132,41 @@ def filt_rep(i, res, nota):
 
 #gera primeira espécie
 def main(cf, contra):
+    global res
     global it
     it = 1
     completou = False
-    abortar = False
-    while completou == False and abortar == False:
-        contra.clear()
+    while completou == False:
+        res = []
         j = 0
         #primeira nota
-        while len(contra.notas) == 0:
+        while len(res) == 0:
             j += 1
             if cf.naipe == 1:
                 nota2 = random.randint(0, 1) * 12 + cf.modo[0]
                 if not filt_tess(nota2, contra.naipe):
-                    contra.add(nota2)
+                    res.append(nota2)
             else:
                 nota2 = random.randint(-1, 0) * 12 + cf.modo[0]
                 if not filt_tess(nota2, contra.naipe):
-                    contra.add(nota2)
+                    res.append(nota2)
             if j == 100:
                 for k in range(2):
                     if cf.naipe == 1:
                         nota2 = k
                         if not filt_tess(nota2, contra.naipe):
-                            contra.add(nota2)
+                            res.append(nota2)
                     else:
                         nota2 = k
                         if not filt_tess(nota2, contra.naipe):
-                            contra.add(nota2)
-                if len(contra.notas) == 0:
+                            res.append(nota2)
+                if len(res) == 0:
                     print("Naipe incompatível")
-                    abortar = True
-                    break
         for i in range(1, len(cf.modo)):
             #última nota
-            if len(contra.notas) == len(cf.modo) - 1:
-                if not filt_pll(i, cf.modo, contra.notas, contra.notas[0]):
-                    contra.add(contra.notas[0])
+            if len(res) == len(cf.modo) - 1:
+                if not filt_pll(i, cf.modo, res, res[0]):
+                    res.append(res[0])
                     completou = True
                     print("\n")
                     print(f"tentativas: {it}")
@@ -172,21 +176,20 @@ def main(cf, contra):
                 nota2 = gerar_nota(contra)
                 #filtros
                 if filt_diss(cf.modo[i], nota2) \
-                or filt_pll(i, cf.modo, contra.notas, nota2)\
-                or filt_rep(i, contra.notas, nota2):
+                or filt_pll(i, cf.modo, res, nota2)\
+                or filt_rep(i, res, nota2):
                     it += 1
                     break
 
                 else:
-                    contra.add(nota2)
+                    res.append(nota2)
         if it >= 1000000:
             print("\nExcedeu limite de iterações")
-            abortar = True
             break
 
 cf_auto()
 print("\n", cf.modo, cf.naipe, contra.naipe,"\n")
 main(cf, contra)
 print(f"cantus firmus:{cf.modo}")
-print(f"resolução:    {contra.notas}")
-print_prim_esp(cf, contra)
+print(f"resolução:    {res}")
+print_prim_esp(cf, contra, res)
