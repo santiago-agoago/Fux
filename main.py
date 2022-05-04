6f2dc513fadf2d34e0416fafb87eab6a67eee445
santiago <sansantitiagoago@gmail.com>
2022-05-03 21:13:53 -0300

    Restauro e cadência
    Restaurando alterações de abril. Reformulações de classes. Cadência do Fux a partir da antepenúltima nota. Claves e vozes como no Fux.
    
    Debug: filtros na antepenúltima nota

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import random
 from dicionario import *
 
-
 class CF:
-    def __init__(self, modo, naipe):
+    def __init__(self, modo, voz):
         self.modo = modo
-        self.naipe = naipe
-    def clave(self):
-        if self.naipe == 1:
-            return "bass"
-        else:
-            return "alto"
+        self.voz = voz
+
 class RES:
-    def __init__(self, naipe):
-        self.naipe = naipe
+    def __init__(self, reg, voz):
+        self.reg = reg
         self.notas = []
+        self.voz = voz
     def add(self, nota):
         self.notas.append(nota)
     def clear(self):
@@ -22,57 +18,73 @@ class RES:
 
 def cf_auto():
     global cf
-    global contra
-    print("\nQual o cantus firmus?\n1. Jônico\n2. Dórico\n3. Frígio\n4. Lídio\n5. Mixolídio\n6. Eólio")
-    entrada = int(input("> "))
+    global cp
+    print("\nQual o cantus firmus?\n1. Dórico\n2. Frígio\n3. Lídio\n4. Mixolídio\n5. Eólio\n6. Jônico")
+    entrada1 = int(input("> "))
     print("\nGerar melodia inferior ou superior?\n1. Superior\n2. Inferior")
-    if entrada == 1:
-        cf = CF(jonico, int(input("> ")))
-    if entrada == 2:
-        cf = CF(dorico, int(input("> ")))
-    if entrada == 3:
-        cf = CF(frigio, int(input("> ")))
-    if entrada == 4:
-        cf = CF(lidio, int(input("> ")))
-    if entrada == 5:
-        cf = CF(mixo, int(input("> ")))
-    if entrada == 6:
-        cf = CF(eolio, int(input("> ")))
-    print("\nPara que naipe?\nS A T B")
-    entrada2 = str(input("> ").lower())
-    if entrada2 == "s":
-        contra = RES(s)
-    if entrada2 == "a":
-        contra = RES(a)
-    if entrada2 == "t":
-        contra = RES(t)
-    if entrada2 == "b":
-        contra = RES(b)
+    entrada2 = int(input("> "))
+    if entrada1 == 1:
+        cf = CF(dorico, a)
+        if entrada2 == 1:
+            cp = RES(2, s)
+        if entrada2 == 2:
+            cp = RES(1, t)
+    if entrada1 == 2:
+        cf = CF(frigio, a)
+        if entrada2 == 1:
+            cp = RES(2, s)
+        if entrada2 == 2:
+            cp = RES(1, t)
+    if entrada1 == 3:
+        cf = CF(lidio, t)
+        if entrada2 == 1:
+            cp = RES(2, a)
+        if entrada2 == 2:
+            cp = RES(1, b)
+    if entrada1 == 4:
+        cf = CF(mixo, a)
+        if entrada2 == 1:
+            cp = RES(2, s)
+        if entrada2 == 2:
+            cp = RES(1, t)
+    if entrada1 == 5:
+        cf = CF(eolio, a)
+        if entrada2 == 1:
+            cp = RES(2, s)
+        if entrada2 == 2:
+            cp = RES(1, t)
+    if entrada1 == 6:
+        cf = CF(jonico, a)
+        if entrada2 == 1:
+            cp = RES(2, s)
+        if entrada2 == 2:
+            cp = RES(1, t)
+
 #print
-def print_prim_esp(cf, contra):
-    if cf.naipe == 1:
-        print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff {\n\\clef ", contra.naipe["clave"])
-        for i in range(len(contra.notas)):
-            print(midi_ly[contra.notas[i]], "1", sep="", end=" ")
+def print_prim_esp(cf, cp):
+    if cp.reg == 2:
+        print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff {\n\\clef ", cp.voz["clave"])
+        for i in range(len(cp.notas)):
+            print(midi_ly[cp.notas[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
-                print("\n}\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef bass")
+                print("\n}\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef", cf.voz["clave"])
         for i in range(len(cf.modo)):
             print(midi_ly[cf.modo[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
                 print("\n}\n>>")
     else:
-        print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef alto")
+        print("\n\\version \"2.22.2\"\n\\language \"portugues\" \n<<\n\\new Staff \with {instrumentName = \"CF\"}\n{\n\\clef", cf.voz["clave"])
         for i in range(len(cf.modo)):
-            print(midi_ly[cf.modo[i] + 12], "1", sep="", end=" ")
+            print(midi_ly[cf.modo[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
-                print("\n}\n\\new Staff {\n\\clef ", contra.naipe["clave"])
-        for i in range(len(contra.notas)):
-            print(midi_ly[contra.notas[i]], "1", sep="", end=" ")
+                print("\n}\n\\new Staff {\n\\clef ", cp.voz["clave"])
+        for i in range(len(cp.notas)):
+            print(midi_ly[cp.notas[i]], "1", sep="", end=" ")
             if i == len(cf.modo) - 1:
                 print("\n}\n>>")
 #quantizador
-def gerar_nota(contra):
-    nota2 = random.randint(contra.naipe["mi"], contra.naipe["ma"])
+def gerar_nota(cp):
+    nota2 = random.randint(cp.voz["mi"], cp.voz["ma"])
     if nota2 not in naturais:
         return nota2 + 1
     else:
@@ -84,98 +96,150 @@ def int_mel(nota, notal):
     return notal - nota
 
 #filtros
-def filt_tess(nota, naipe):
-    if nota > naipe["ma"] or nota < naipe["mi"]:
+    #regras melódicas
+def filt_tess(nota, voz):
+    if nota > voz["ma"] or nota < voz["mi"]:
         return True
-def filt_diss(nota1, nota2):
-    if abs(nota1 - nota2) not in int_cons:
+def filt_rep(i, res, nota):
+    if i > 1 and nota == res[i - 1] and nota == res[i - 2]:
         return True
-def filt_pll(i, cf, res, nota):
-    if int_har(cf[i - 1], res[i - 1]) in conper:
-        if int_har(cf[i], nota) in conper:
-            return True
-
-        if int_mel(res[i - 1], nota) >= 1 and int_mel(cf[i - 1], cf[i]) >= 1:
-            return True
-        if int_mel(res[i - 1], nota) <= -1 and int_mel(cf[i - 1], cf[i]) <=-1:
-            return True
 def filt_meldis(i, res, nota):
     if int_mel(res[i -1], nota) in int_meldis:
         return True
     if int_har(res[i - 1], nota) > 12:
         return True
+    #regras harmônicas
+def filt_diss(nota1, nota2):
+    if abs(nota1 - nota2) not in int_cons:
+        return True
+    #regras combinadas (Hiller, 1959)
+def filt_pll(i, cf, res, nota):
+    if int_har(cf[i], nota) in conper:
+        if int_mel(res[i - 1], nota) >= 1 and int_mel(cf[i - 1], cf[i]) >= 1:
+            return True
+        if int_mel(res[i - 1], nota) <= 1 and int_mel(cf[i - 1], cf[i]) <= -1:
+            return True
 
 
+#cadência primeira espécie
+def cad_prim(cf, cp, i):
+# cadência tipo 1
+    if cf.modo == dorico or cf.modo == eolio:
+        if cp.reg == 2:
+            nota2 = cf.modo[len(cf.modo) - 1] + 12
+            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                cp.add(nota2)
+        else:
+            nota2 = cf.modo[len(cf.modo) - 1]
+            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                cp.add(nota2)
 
-def filt_rep(i, res, nota):
-    if nota == res[i - 1]:
+# cadência tipo 1 ou tipo 2
+    elif cf.modo == frigio:
+        if cp.reg == 2:
+            nota2 = cf.modo[len(cf.modo) - 1] + 12
+            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                cp.add(nota2)
+        else:
+            np = [cf.modo[len(cf.modo) - 1] - 4, cf.modo[len(cf.modo) - 1]]
+            random.shuffle(np)
+            while len(cp.notas) < len(cf.modo) - 2:
+                for i in range(2):
+                    nota2 = np[i]
+                    if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                        cp.add(nota2)
+                    if i > 2:
+                        break
+    else:
+        if cp.reg == 2:
+            nota2 = cf.modo[len(cf.modo) - 1] + 12
+            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                cp.add(nota2)
+        else:
+            np = [cf.modo[len(cf.modo) - 1] - 3, cf.modo[len(cf.modo) - 1]]
+            random.shuffle(np)
+            while len(cp.notas) < len(cf.modo) - 2:
+                for i in range(2):
+                    nota2 = np[i]
+                    if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                        cp.add(nota2)
+                    if i > 2:
+                        break
+
+# penúltima e última nota
+    if cp.reg == 2:
+        nota2 = cf.modo[len(cf.modo) - 2] + 9
+        cp.add(nota2)
+        cp.add(cf.modo[len(cf.modo) - 1] + 12)
+        return True
+    else:
+        nota2 = cf.modo[len(cf.modo) - 2] - 3
+        cp.add(nota2)
+        cp.add(cf.modo[len(cf.modo) - 1])
         return True
 
-
 #gera primeira espécie
-def main(cf, contra):
+def main(cf, cp):
     global it
     it = 1
     completou = False
     abortar = False
     while completou == False and abortar == False:
-        contra.clear()
+        cp.clear()
         j = 0
         #primeira nota
-        while len(contra.notas) == 0:
+        while len(cp.notas) == 0:
             it += 1
             j += 1
-            if cf.naipe == 1:
-                nota2 = random.randint(0, 1) * 12 + cf.modo[0]
-                if not filt_tess(nota2, contra.naipe):
-                    contra.add(nota2)
+
+            if cp.reg == 1:
+                random.shuffle(prim_nota_sup)
+                for i in range(3):
+                    nota2 = (cf.modo[0] + prim_nota_sup[i])
+                    if not filt_tess(nota2, cp.voz):
+                        cp.add(nota2)
+                        break
             else:
-                nota2 = random.randint(-1, 0) * 12 + cf.modo[0]
-                if not filt_tess(nota2, contra.naipe):
-                    contra.add(nota2)
-            if j == 100:
-                for k in range(2):
-                    if cf.naipe == 1:
-                        nota2 = k
-                        if not filt_tess(nota2, contra.naipe):
-                            contra.add(nota2)
-                    else:
-                        nota2 = k
-                        if not filt_tess(nota2, contra.naipe):
-                            contra.add(nota2)
-                if len(contra.notas) == 0:
-                    print("Naipe incompatível")
-                    abortar = True
-                    break
+                random.shuffle(prim_nota_inf)
+                for i in range(2):
+                    nota2 = cf.modo[0] + prim_nota_inf[i]
+                    if not filt_tess(nota2, cp.voz):
+                        cp.add(nota2)
+                        break
+            if len(cp.notas) == 0:
+                print("Voz incompatível")
+                abortar = True
+                break
+        #principal
         for i in range(1, len(cf.modo)):
-            #última nota
-            if len(contra.notas) == len(cf.modo) - 1:
-                if not filt_pll(i, cf.modo, contra.notas, contra.notas[0]):
-                    contra.add(contra.notas[0])
+            #CADENCIA
+            if i == len(cf.modo) - 3:
+                #sub-rotina de cadência
+                if cad_prim(cf, cp, i):
                     completou = True
                     print("\n")
                     print(f"tentativas: {it}")
                     break
 
             else:
-                nota2 = gerar_nota(contra)
+                nota2 = gerar_nota(cp)
                 #filtros
                 if filt_diss(cf.modo[i], nota2) \
-                or filt_pll(i, cf.modo, contra.notas, nota2)\
-                or filt_rep(i, contra.notas, nota2)\
-                or filt_meldis(i, contra.notas, nota2):
+                or filt_pll(i, cf.modo, cp.notas, nota2)\
+                or filt_rep(i, cp.notas, nota2)\
+                or filt_meldis(i, cp.notas, nota2):
                     it += 1
                     break
 
                 else:
-                    contra.add(nota2)
+                    cp.add(nota2)
         if it >= 5000000:
             print("\nExcedeu limite de iterações")
             abortar = True
             break
 
 cf_auto()
-print("\n", cf.modo, cf.naipe, contra.naipe)
-main(cf, contra)
-print(f"cantus firmus:{cf.modo}\nresolução:    {contra.notas}\niterações:    {it}")
-print_prim_esp(cf, contra)
+print("\n", cf.modo, cf.voz, "\n", cp.reg, cp.voz)
+main(cf, cp)
+print(f"cantus firmus:{cf.modo}\nresolução:    {cp.notas}\niterações:    {it}")
+print_prim_esp(cf, cp)
--- teste.py
+++ teste.py
@@ -1,8 +1,60 @@
-lista1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-import random
+from dicionario import *
 
-def gerar_nota():
-    return random.randint(0, 10)
+class CF:
+    def __init__(self, modo, voz):
+        self.modo = modo
+        self.voz = voz
 
-nota = gerar_nota()
-print(nota)
+class RES:
+    def __init__(self, reg, voz):
+        self.reg = reg
+        self.notas = []
+        self.voz = voz
+    def add(self, nota):
+        self.notas.append(nota)
+    def clear(self):
+        self.notas = []
+
+def cf_auto():
+    global cf
+    global contra
+    print("\nQual o cantus firmus?\n1. Dórico\n2. Frígio\n3. Lídio\n4. Mixolídio\n5. Eólio\n6. Jônico")
+    entrada1 = int(input("> "))
+    print("\nGerar melodia inferior ou superior?\n1. Superior\n2. Inferior")
+    entrada2 = int(input("> "))
+    if entrada1 == 1:
+        cf = CF(dorico, "alto")
+        if entrada2 == 1:
+            contra = RES(2, "soprano")
+        if entrada2 == 2:
+            contra = RES(1, "tenor")
+    if entrada1 == 2:
+        cf = CF(frigio,"alto")
+        if entrada2 == 1:
+            contra = RES(2, "soprano")
+        if entrada2 == 2:
+            contra = RES(1, "tenor")
+    if entrada1 == 3:
+        cf = CF(lidio, "tenor")
+        if entrada2 == 1:
+            contra = RES(2, "alto")
+        if entrada2 == 2:
+            contra = RES(1, "bass")
+    if entrada1 == 4:
+        cf = CF(mixo, "alto")
+        if entrada2 == 1:
+            contra = RES(2, "soprano")
+        if entrada2 == 2:
+            contra = RES(1, "tenor")
+    if entrada1 == 5:
+        cf = CF(eolio, "alto")
+        if entrada2 == 1:
+            contra = RES(2, "soprano")
+        if entrada2 == 2:
+            contra = RES(1, "tenor")
+    if entrada1 == 6:
+        cf = CF(jonico, "alto")
+        if entrada2 == 1:
+            contra = RES(2, "soprano")
+        if entrada2 == 2:
+            contra = RES(1, "tenor")
