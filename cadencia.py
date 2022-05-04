f1a9eb688266149305374a671dc94ab0909a48e4
santiago <sansantitiagoago@gmail.com>
2022-05-04 19:54:07 -0300

    Debug cadencia

--- cadencia.py
+++ cadencia.py
@@ -7,40 +7,44 @@ def cad_prim(cf, cp, i):
     if cf.modo == dorico or cf.modo == eolio:
         if cp.reg == 2:
             nota2 = cf.modo[len(cf.modo) - 1] + 12
-            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+            if not filt_pll(i, cf.modo, cp.notas, nota2) and not filt_meldis(i, cp.notas, nota2) and not filt_rep(i, cp.notas, nota2):
                 cp.add(nota2)
 
         else:
             nota2 = cf.modo[len(cf.modo) - 1]
-            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+            if not filt_pll(i, cf.modo, cp.notas, nota2) and not filt_meldis(i, cp.notas, nota2) and not filt_rep(i, cp.notas, nota2):
                 cp.add(nota2)
 
 # cadência tipo 1 ou tipo 2
     elif cf.modo == frigio:
+        #tipo 1
         if cp.reg == 2:
             nota2 = cf.modo[len(cf.modo) - 1] + 12
-            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+            if not filt_pll(i, cf.modo, cp.notas, nota2) and not filt_meldis(i, cp.notas, nota2) and not filt_rep(i, cp.notas, nota2):
                 cp.add(nota2)
+        #tipo 2
         else:
             np = [cf.modo[len(cf.modo) - 1] - 4, cf.modo[len(cf.modo) - 1]]
             random.shuffle(np)
             for i in range(2):
                 nota2 = np[i]
-                if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                if not filt_pll(i, cf.modo, cp.notas, nota2) and not filt_meldis(i, cp.notas, nota2) and not filt_rep(i, cp.notas, nota2):
                     cp.add(nota2)
                     break
 
     else:
+        #tipo 1
         if cp.reg == 2:
             nota2 = cf.modo[len(cf.modo) - 1] + 12
-            if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+            if not filt_pll(i, cf.modo, cp.notas, nota2) and not filt_meldis(i, cp.notas, nota2) and not filt_rep(i, cp.notas, nota2):
                 cp.add(nota2)
+        #tipo 2
         else:
             np = [cf.modo[len(cf.modo) - 1] - 3, cf.modo[len(cf.modo) - 1]]
             random.shuffle(np)
             for i in range(2):
                 nota2 = np[i]
-                if not filt_pll(i, cf.modo, cp.notas, nota2) or filt_meldis(i, cp.notas, nota2) or filt_rep(i, cp.notas, nota2):
+                if not filt_pll(i, cf.modo, cp.notas, nota2) and not filt_meldis(i, cp.notas, nota2) and not filt_rep(i, cp.notas, nota2):
                     cp.add(nota2)
                     break
 
