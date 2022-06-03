2c95671333cd0d6ead82b01daff86a9ff370478b
santiago <sansantitiagoago@gmail.com>
2022-06-03 17:50:29 -0300

    Cadência transposta
    O contraponto termina uma oitava abaixo do CF caso o intervalo de passagem para a cadência seja menor

--- cadencia.py
+++ cadencia.py
@@ -7,6 +7,50 @@ def filtros(i, cf, cp, nota):
             or filt_saltos(i, cp.notas, nota):
         return True
 
+def final_tipo1(i, cf, cp, ante):
+    if cp.reg == 2:
+        cp.add(ante)
+        cp.add(cf.modo[len(cf.modo) - 2] + 9)
+        cp.add(ante)
+    else:
+        if int_har(ante, cp.notas[-1]) > 6 and int_har(ante - 12, cp.notas[-1]) < 6:
+            if not filtros(i, cf, cp, ante):
+                cp.add(ante - 12)
+                cp.add(cf.modo[len(cf.modo) - 2] - 15)
+                cp.add(ante - 12)
+            else:
+                cp.add(ante)
+                cp.add(cf.modo[len(cf.modo) - 2] - 3)
+                cp.add(ante)
+
+        else:
+            cp.add(ante)
+            cp.add(cf.modo[len(cf.modo) - 2] - 3)
+            cp.add(ante)
+
+
+def final_tipo2(i, cf, cp, ante):
+    if cp.reg == 2:
+        cp.add(ante)
+        cp.add(cf.modo[len(cf.modo) - 2] + 9)
+        cp.add(cf.modo[len(cf.modo) - 1] + 12)
+    else:
+        if int_har(ante, cp.notas[-1]) > 6 and int_har(ante - 12, cp.notas[-1]) < 6:
+            if not filtros(i, cf, cp, ante):
+                cp.add(ante - 12)
+                cp.add(cf.modo[len(cf.modo) - 2] - 15)
+                cp.add(cf.modo[len(cf.modo) - 1] - 12)
+            else:
+                cp.add(ante)
+                cp.add(cf.modo[len(cf.modo) - 2] - 3)
+                cp.add(cf.modo[len(cf.modo) - 1])
+
+        else:
+            cp.add(ante)
+            cp.add(cf.modo[len(cf.modo) - 2] - 3)
+            cp.add(cf.modo[len(cf.modo) - 1])
+
+
 
 
 #cadência primeira espécie
@@ -17,13 +61,15 @@ def cad_prim(cf, cp, i):
         if cp.reg == 2:
             nota2 = cf.modo[len(cf.modo) - 1] + 12
             if not filtros(i, cf, cp, nota2):
-                cp.add(nota2)
+                final_tipo1(i, cf, cp, nota2)
+                return True
 
         else:
             # IF
             nota2 = cf.modo[len(cf.modo) - 1]
             if not filtros(i, cf, cp, nota2):
-                cp.add(nota2)
+                final_tipo1(i, cf, cp, nota2)
+                return True
 
 # cadência tipo 1 ou tipo 2
     elif cf.modo == frigio:
@@ -31,7 +77,9 @@ def cad_prim(cf, cp, i):
         if cp.reg == 2:
             nota2 = cf.modo[len(cf.modo) - 1] + 12
             if not filtros(i, cf, cp, nota2):
-                cp.add(nota2)
+                final_tipo1(i, cf, cp, nota2)
+                return True
+
         #tipo 2
 
         else:
@@ -41,15 +89,18 @@ def cad_prim(cf, cp, i):
             for i in range(2):
                 nota2 = np[i]
                 if not filtros(i, cf, cp, nota2):
-                    cp.add(nota2)
+                    final_tipo2(i, cf, cp, nota2)
+                    return True
                     break
 
+    # frigio, mixo, jonico
     else:
         #tipo 1
         if cp.reg == 2:
             nota2 = cf.modo[len(cf.modo) - 1] + 12
             if not filtros(i, cf, cp, nota2):
-                cp.add(nota2)
+                final_tipo1(i, cf, cp, nota2)
+                return True
         #tipo 2
         else:
             # IF
@@ -58,22 +109,23 @@ def cad_prim(cf, cp, i):
             for i in range(2):
                 nota2 = np[i]
                 if not filtros(i, cf, cp, nota2):
-                    cp.add(nota2)
+                    final_tipo2(i, cf, cp, nota2)
+                    return True
                     break
 
 # EXCLUIR VVVVV
 # penúltima e última nota
-    if len(cp.notas) == len(cf.modo) - 2:
-        if cp.reg == 2:
-            nota2 = cf.modo[len(cf.modo) - 2] + 9
-            cp.add(nota2)
-            cp.add(cf.modo[len(cf.modo) - 1] + 12)
-            return True
-        #VERIFICAR OITAVA
-        else:
-            nota2 = cf.modo[len(cf.modo) - 2] - 3
-            cp.add(nota2)
-            cp.add(cf.modo[len(cf.modo) - 1])
-            return True
-    else:
-        return False
+#    if len(cp.notas) == len(cf.modo) - 2:
+#        if cp.reg == 2:
+#           nota2 = cf.modo[len(cf.modo) - 2] + 9
+#            cp.add(nota2)
+#            cp.add(cf.modo[len(cf.modo) - 1] + 12)
+#            return True
+#        #VERIFICAR OITAVA
+#        else:
+#            nota2 = cf.modo[len(cf.modo) - 2] - 3
+#            cp.add(nota2)
+#            cp.add(cf.modo[len(cf.modo) - 1])
+#            return True
+#   else:
+#        return False
---
+++ testador.py
@@ -0,0 +1,59 @@
+from entrada import *
+from main import *
+import timeit
+from timeit import repeat
+from datetime import date
+import subprocess
+
+dados = open("dados.txt", "a", encoding = "utf-8")
+
+def get_git() -> str:
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+
+def run(cf, cp):
+    global it
+    # print(cf.modo, cf.voz)
+    # print(cp.reg, cp.voz)
+    it = main(cf, cp)
+    print(f"|cantus firmus:{cf.modo}\n|resolução:    {cp.notas}")
+    print_prim_esp(cf, cp,)
+    print()
+
+def teste():
+    it_total = 0
+    start = timeit.default_timer()
+    for key, value in cantus_firmi.items():
+        if value == lidio:
+            cf = CF(value, t)
+            print(f"\n–> {key}\n")
+            for j in range(2):
+                if j == 0:
+                    cp = RES(j + 1, b)
+                    run(cf, cp)
+                    it_total += it
+                else:
+                    cp = RES(j + 1, a)
+                    run(cf, cp)
+                    it_total += it
+                    print("_________________________________________________")
+
+        else:
+            cf = CF(value, a)
+            print(f"\n–> {key}\n")
+            for j in range(2):
+                if j == 0:
+                    cp = RES(j + 1, t)
+                    run(cf, cp)
+                    it_total += it
+                else:
+                    cp = RES(j + 1, s)
+                    run(cf, cp)
+                    it_total += it
+                    print("_________________________________________________")
+    stop = timeit.default_timer()
+    tempo_total = round(stop - start, 5)
+    print(f"TEMPO TOTAL (s): {tempo_total}")
+    print(f"ITERAÇÕES TOTAIS: {it_total}")
+    dados.write(f"{date.today()} git: {get_git()} / iterações: {it_total} t = {str(tempo_total)}\n")
+
+teste()
