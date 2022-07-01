e58eb228fa9c9fef68537f12551d160cec5a785e
santiago <sansantitiagoago@gmail.com>
2022-07-01 12:14:56 -0300

    plot média móvel

--- analise.py
+++ analise.py
@@ -4,14 +4,42 @@ import statistics
 
 dados = open("dados.txt", "r")
 
-x = []
-y = []
+x_vb = []
+y_vb = []
+git1 = 0
+x = 0
+#for line in dados:
+#    x += 1
+#    x_vb.append(x)
+#    for i in range(len(line)):
+#        if line[i] == "t":
+#            y = i + 4
+#    y_vb.append(float(line[y:-1]))
+
+#print(x_vb, y_vb)
+
+x_vm =[]
+y_vm = []
+y_p = []
+x = 0
 
 for line in dados:
-    x.append(line[16:23])
-    for i in range(len(line)):
-        if line[i] == "t":
-            y_i = i + 4
-    y.append(line[y_i:y_i + 4])
+    git2 = line[16:23]
 
-print(x, y)
+    for i in range(len(line)):
+        if line[i] == "=":
+            y = i + 2
+
+    y_p.append(float(line[y:-1]))
+
+    if git2 != git1:
+        x += 0.01
+        x_vm.append(x)
+        git1 = git2
+        y_vm.append(statistics.mean(y_p))
+        y_p = []
+
+print(x_vm, y_vm)
+
+plt.plot(x_vm, y_vm)
+plt.show()
